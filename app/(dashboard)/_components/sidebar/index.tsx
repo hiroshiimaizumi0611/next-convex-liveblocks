2a30a44e5c3d0e4f1a97dd4c07a1a9e88570a2f1
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-04-29 13:36:58 +0900

    base layout

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+  return (
+    <aside className="fixed h-full z-[1] left-0 w-[60px] bg-blue-950 flex p-3 flex-col gap-y-4 text-white">
+      Side
+    </aside>
+  );
+};
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <div>Dashboard Root</div>;
+};
+
+export default DashboardPage;
