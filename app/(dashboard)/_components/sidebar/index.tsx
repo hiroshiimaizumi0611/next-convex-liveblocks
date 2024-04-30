cfd8d9151f9ed7fbd5e5944dc7c93ffde7f36017
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-04-30 10:19:38 +0900

    sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,11 @@
+import { List } from "./list";
+import { NewButton } from "./new-button";
+
 export const Sidebar = () => {
   return (
     <aside className="fixed h-full z-[1] left-0 w-[60px] bg-blue-950 flex p-3 flex-col gap-y-4 text-white">
-      Side
+      <List />
+      <NewButton />
     </aside>
   );
 };
