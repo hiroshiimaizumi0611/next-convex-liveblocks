7710f3d13e3ad45b195cc16a8782e9ad44727c92
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-05-01 10:47:07 +0900

    dashboard

---
+++ app/(dashboard)/_components/empty-org.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { CreateOrganization } from "@clerk/nextjs";
+import Image from "next/image";
+
+export const EmptyOrg = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src={"/logo.svg"} alt={"Logo"} height={200} width={200} />
+      <h2 className="text-2xl font-semibold mt-6">Welcome to Board</h2>
+      <p className="text-sm text-muted-foreground mt-2">Create an organization to get started</p>
+      <div className="mt-6">
+        <Dialog>
+            <DialogTrigger>
+                <Button size={"lg"}>
+                    Create organization
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="max-w-[500px]">
+                <CreateOrganization/>
+            </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
