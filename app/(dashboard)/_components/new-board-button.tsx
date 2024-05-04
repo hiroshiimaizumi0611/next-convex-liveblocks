cd7accb273dac97d8551bdecbd2fffe2111d8fd4
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-05-04 15:58:10 +0900

    boardList boardCard

---
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -0,0 +1,41 @@
+"use clirnt";
+
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { cn } from "@/lib/utils";
+import { Plus } from "lucide-react";
+import { toast } from "sonner";
+
+interface NewBoardButtonProps {
+  orgId: string;
+  disabled?: boolean;
+}
+
+export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    mutate({
+      orgId,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
+  return (
+    <button
+      disabled={pending || disabled}
+      onClick={onClick}
+      className={cn(
+        "bg-blue-600 rounded-lg hover:bg-blue-800 col-span-1 aspect-[100/127] flex flex-col items-center justify-center py-6",
+        (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
+      )}
+    >
+      <Plus className="stroke-1 text-white h-12 w-12" />
+      <p className="text-sm text-white font-light">New Board</p>
+    </button>
+  );
+};
