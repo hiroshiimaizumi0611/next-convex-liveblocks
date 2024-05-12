592c068f200fadede9c1f3a23466cfabd5eba38b
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-05-12 14:56:19 +0900

    room info

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -11,10 +11,10 @@ interface CanvasProps {
 
 export const Canvas = ({ boardId }: CanvasProps) => {
   const info = useSelf((me) => me.info);
-  
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId}/>
       <Participants />
       <Toolbar />
     </main>
